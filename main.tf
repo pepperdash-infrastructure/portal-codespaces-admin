66fa74af9e22390626fb00f87109fd775a3b0793
Andrew Welker <awelker@pepperdash.com>
2023-12-29 14:18:53 -0600

    fix: remove AWS stuff for now

--- main.tf
+++ main.tf
@@ -4,25 +4,10 @@ terraform {
       source = "spacelift-io/spacelift"
       version = "1.8.0"
     }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.31.0"
-    }
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
 provider "spacelift" {}
-
-data "aws_caller_identity" "current" {}
-
-locals {
-  role_name = "spacelift-${var.user_name}"
-  role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.role_name}"
-}
 
 resource "spacelift_stack" "portal_codespaces_secrets" {
   name = "${var.user_name}-portal-codespaces-secrets"
@@ -30,11 +15,5 @@ resource "spacelift_stack" "portal_codespaces_secrets" {
   repository = "portal-codespaces" 
   branch = "main"
   space_id = var.space_id
+  
 }
-
-resource "spacelift_aws_integration" "aws_integration" {
-  name = local.role_name
-  role_arn = local.role_arn
-  generate_credentials_in_worker = true
-}
-
